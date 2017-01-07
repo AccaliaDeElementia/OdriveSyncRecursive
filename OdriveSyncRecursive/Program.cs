ffcbedc4c17924c06ccd4c1e1c4372b341239a35
Accalia Elementia <accalia.de.elementia@gmail.com>
2017-01-07 07:57:40 -0800

    initial commit of source with folder walking option

---
+++ OdriveSyncRecursive/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdriveSyncRecursive
+{
+    class Program
+    {
+        private delegate Task HandleDirectory(IEnumerable<DirectoryInfo> dirs, IEnumerable<FileInfo> files);
+
+        private static void WalkPath(DirectoryInfo basePath, HandleDirectory handler)
+        {
+            var paths = new Queue<DirectoryInfo>(new []{basePath});
+            while (paths.Any())
+            {
+                var path = paths.Dequeue();
+                var dirs = path.EnumerateDirectories();
+                var files = path.EnumerateFiles();
+                handler(dirs, files).Wait();
+                foreach (var dir in path.EnumerateDirectories().OrderBy(d => d.Name))
+                {
+                    paths.Enqueue(dir);
+                }                
+            }
+            
+        }
+
+        private static async Task WalkLog(IEnumerable<DirectoryInfo> dirs, IEnumerable<FileInfo> files)
+        {
+            await Task.Delay(0);
+            foreach (var file in files)
+            {
+                Console.WriteLine(file.FullName);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            WalkPath(new DirectoryInfo(@"C:\Users\accalia"), WalkLog);
+        }
+    }
+}
